2b869565c1ed304827532fd225923ec73d897ec6
OlivierNDO <Olivier_N@lynchburg.edu>
2020-05-20 18:29:58 -0500

    data processing configuration file

---
+++ configuration/data_processing_configuration.py
@@ -0,0 +1,35 @@
+
+
+
+
+
+### Object Classes to Use in Data Processing
+###############################################################################
+config_obj_detection_classes = ['Bathtub', 'Countertop', 'Cabinetry','Ceiling fan',
+                                'Coffee table', 'Couch', 'Desk', 'Dishwasher', 'Fireplace',
+                                'Furniture', 'Heater', 'Jacuzzi', 'Kitchen & dining room table',
+                                'Loveseat', 'Microwave oven', 'Nightstand', 'Oven', 'Piano',
+                                'Refrigerator', 'Table', 'Television', 'Toilet',
+                                'Training bench', 'Treadmill', 'Washing machine']
+
+
+
+### File Paths
+###############################################################################
+raw_data_dir = ''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
