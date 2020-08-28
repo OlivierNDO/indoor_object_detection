22af311721b0bf3031bf381ed603dc3585790882
OlivierNDO <Olivier_N@lynchburg.edu>
2020-08-28 16:41:18 -0500

    in progress class object

--- data_processing/proc_open_images.py
+++ data_processing/proc_open_images.py
@@ -1,19 +1,28 @@
 ### Configuration
 ###############################################################################
 # Import Modules
+import datetime
 from io import BytesIO
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import pandas as pd
 from PIL import Image
 import requests
 from tensorflow.keras.preprocessing.image import load_img
+import time
 from skimage.transform import resize
 
+# File Paths
+config_data_dir = 'D:/indoor_object_detection/data/source_data/'
+config_proc_data_dir = 'D:/indoor_object_detection/data/processed_data/'
+config_train_image_csv = 'train-images-boxable-with-rotation.csv'
+config_train_annotations_csv = 'train-annotations-human-imagelabels-boxable.csv'
+config_train_bbox_csv = 'oidv6-train-annotations-bbox.csv'
+config_class_desc_csv = 'class-descriptions-boxable.csv'
 
 
-
-### Configuration
+### Define Functions
 ###############################################################################
 
 def img_add_flip(arr, flip_horiz = True, flip_vert = False):
@@ -65,43 +74,105 @@ def read_url_image(url):
     return np.array(img)
 
 
+def print_timestamp_message(message, timestamp_format = '%Y-%m-%d %H:%M:%S'):
+    """
+    Print formatted timestamp followed by custom message
+    Args:
+        message (str): string to concatenate with timestamp
+        timestamp_format (str): format for datetime string. defaults to '%Y-%m-%d %H:%M:%S'
+    """
+    ts_string = datetime.datetime.fromtimestamp(time.time()).strftime(timestamp_format)
+    print(f'{ts_string}: {message}')
+
+
+def create_folder_if_not_existing(folder_path):
+    """
+    Use os to create a folder if it does not already exist on the machien
+    Args:
+        folder_path (str): folder path to conditionally create
+    """
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print_timestamp_message(f'Creating folder path {folder_path}')
+    else:
+        print_timestamp_message(f'Folder path {folder_path} already exists')
 
 
 
 
+### Define Functions
+###############################################################################
+
+#os.listdir(config_data_dir)
+train_img_df = pd.read_csv(f'{config_data_dir}{config_train_image_csv}')
+train_annot_df = pd.read_csv(f'{config_data_dir}{config_train_annotations_csv}')
+class_desc_df = pd.read_csv(f'{config_data_dir}{config_class_desc_csv}', header = None)
+class_label_dict = dict(zip(class_desc_df[1], class_desc_df[0]))
 
 
 
 
-
-
-
-url = 'https://farm3.staticflickr.com/5310/5898076654_51085e157c_o.jpg'
-temp = read_url_image(url)
-plt.imshow(temp)
-
-
-
-
-
-
+### Execute Functions
+###############################################################################
+example_class = 'Computer monitor'
+example_label = class_label_dict.get(example_class)
+example_class_image_ids = list(train_annot_df[(train_annot_df.LabelName == example_label) & (train_annot_df.Confidence == 1)]['ImageID'])
+example_class_image_df = train_img_df[train_img_df.ImageID.isin(example_class_image_ids)]
+example_class_image_urls = list(example_class_image_df['Thumbnail300KURL'])
+example_class_bbox_df = pd.read_csv(f'{config_data_dir}{config_train_bbox_csv}')
 
 
 
 
+class OpenCVImageClassRetriever:
+    def __init__(self, 
+                 class_name,
+                 img_csv_path = f'{config_data_dir}{config_train_image_csv}',
+                 annotation_csv_path = f'{config_data_dir}{config_train_annotations_csv}',
+                 bbox_csv_path = f'{config_data_dir}{config_train_bbox_csv}',
+                 class_desc_csv_path = f'{config_data_dir}{config_class_desc_csv}',
+                 output_path = config_proc_data_dir):
+        self.class_name = class_name
+        self.img_csv_path = img_csv_path
+        self.annotation_csv_path = annotation_csv_path
+        self.bbox_csv_path = bbox_csv_path
+        self.class_desc_csv_path = class_desc_csv_path
+    
+    def get_image_class_info(self):
+        # Read Csv Files
+        img_df = pd.read_csv(self.img_csv_path)
+        annot_df = pd.read_csv(self.annotation_csv_path)
+        class_desc_df = pd.read_csv(self.class_desc_csv_path, header = None)
+        class_bbox_df = pd.read_csv(self.bbox_csv_path)
+        
+        # Create Dictionary of Image Class Labels
+        class_label_dict = dict(zip(class_desc_df[1], class_desc_df[0]))
+        
+        # Retrieve and return URLs, Bounding Box DataFrame, and Image IDs
+        class_label = class_label_dict.get(self.class_name)
+        class_image_ids = list(annot_df[(annot_df.LabelName == class_label) & (annot_df.Confidence == 1)]['ImageID'])
+        class_image_df = img_df[img_df.ImageID.isin(class_image_ids)]
+        class_bbox_df = class_bbox_df[class_bbox_df.ImageID.isin(class_image_ids)]
+        class_image_urls = list(class_image_df['Thumbnail300KURL'])
+        return class_image_urls, class_bbox_df, class_image_ids
+
+        
 
 
 
+#image_retriever = OpenCVImageClassRetriever(class_name = 'Computer monitor')
+#temp_urls, temp_bbox_df, image_ids = image_retriever.get_image_class_info()
 
 
 
+#def save_np_array_as_img(img_array, save_name):
+#    """
+#    
+#    """
 
 
 
-
-
-
-
+#example_class_image_urls = train_img_df[train_img_df.ImageID.isin(example_class_image_ids)]
 
 
 
