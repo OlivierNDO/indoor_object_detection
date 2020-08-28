be354d7500c0f433aef6eb5f553fe394ba2b9fc5
OlivierNDO <Olivier_N@lynchburg.edu>
2020-08-28 14:34:25 -0500

    read_url_image function

--- data_processing/proc_open_images.py
+++ data_processing/proc_open_images.py
@@ -1,11 +1,100 @@
-
-# Configuration
+### Configuration
 ###############################################################################
-# Import Packages
-import pandas as pd
-
 # Import Modules
-from indoor_object_detection.src import config_data_processing as cdp
+from io import BytesIO
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from PIL import Image
+import requests
+from tensorflow.keras.preprocessing.image import load_img
+from skimage.transform import resize
+
+
+
+
+### Configuration
+###############################################################################
+
+def img_add_flip(arr, flip_horiz = True, flip_vert = False):
+    """
+    Flip numpy array horizontally and/or vertically
+    Args:
+        arr: three dimensional numpy array
+        flip_horiz: flip image horizontally
+        flip_vert: flip image vertically
+    """
+    assert len(arr.shape) == 3, "'arr' input array must be three dimensional"
+    arr_copy = arr.copy()
+    if flip_horiz:
+        arr_copy = np.fliplr(arr_copy)
+    if flip_vert:
+        arr_copy = np.flipud(arr_copy)
+    return arr_copy
+
+
+def load_resize_images(full_file_paths, resize_height, resize_width):
+    """
+    Load images and resize according to function arguments
+    Args:
+        full_file_paths: list of saved image files
+        resize_height: height of resized output images
+        resize_width: width of resized output images
+    Depdendencies:
+        numpy
+        skimage.transform.resize
+        tensorflow.keras.preprocessing.image.load_img
+    Returns:
+        numpy array of resized images
+    """
+    read_images = [load_img(c) for c in full_file_paths]
+    resized_images = [resize(np.array(ri), (resize_height, resize_width)) for ri in read_images]
+    return np.array(resized_images)
+
+
+def read_url_image(url):
+    """
+    Read image from URL with .jpg or .png extension
+    Args:
+        url (str): url character string
+    Returns:
+        numpy array
+    """
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    return np.array(img)
+
+
+
+
+
+
+
+
+
+
+
+
+
+url = 'https://farm3.staticflickr.com/5310/5898076654_51085e157c_o.jpg'
+temp = read_url_image(url)
+plt.imshow(temp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
